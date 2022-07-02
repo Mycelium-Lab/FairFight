348a48000364a3ff8c3807bcd68283aba3fd0b92
limenal <lshrims@gmail.com>
2022-07-03 02:20:59 +0500

    add wallet deploy

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,7 @@ if (!window.location.search) {
       const provider = window.ton;  
       const accounts = await provider.send('ton_requestAccounts');
       const account = accounts[0];
+      localStorage.setItem("userTonWalletAddress", account)
       walletConnect.textContent = account.slice(0, 6) + '...' + account.slice(account.length - 4, account.length);
       const wallet = tonweb.wallet.create({address: account});
       const address = await wallet.getAddress();
@@ -31,49 +32,108 @@ if (!window.location.search) {
       const internalAddress = await internalWallet.getAddress();
       const internalBalance = await tonweb.getBalance(internalAddress);
       const internalNumBalance = internalBalance / 10**9
-      if (internalNumBalance >= 1) {
+      document.getElementById("walletHeader").style.display = "none"
+      if (internalNumBalance > 0) {
         const text = "Your in-game balance: " + internalNumBalance.toString() + " TON"
-        document.getElementById("walletHeader").innerText = text  
+        document.getElementById("startInternalBalance").style.display = 'inline-block'  
+        document.getElementById("startInternalBalance").innerText = text  
+        document.getElementById("withdrawInput").style.display = "inline-block"
         document.getElementById("room").style.display = "flex"
         document.getElementById("name").disabled = false;
         document.getElementById("inputName").disabled = false;
         document.getElementById("createGame").disabled = false;
         document.getElementById("connect").disabled = true;
-      } else {
-        const text = 'Your balance: ' + (balanceA / 10**9).toString() + ' TON'
-        document.getElementById("walletHeader").innerText = text  
+        const withdrawButton = document.getElementById("withdraw")
+        withdrawButton.style.display = "inline-block"
+        withdrawButton.addEventListener("click", async function() {
+          try {
+            const base64Key = localStorage.getItem("secretKey")
+            const bytesArray = TonWeb.utils.hexToBytes(base64Key)
+            const internalWallet = tonweb.wallet.create({address: localStorage.getItem("walletAddress")})
+            const seqno = await internalWallet.methods.seqno().call()
+            const amount = document.getElementById("withdrawInput").value
+            const transfer = internalWallet.methods.transfer({
+              secretKey: bytesArray,
+              toAddress: localStorage.getItem("userTonWalletAddress"),
+              amount: TonWeb.utils.toNano(amount.toString()),
+              seqno: seqno,
+              payload: 'Withdraw',
+              sendMode: 3,
+            });
+            const transferSended = await transfer.send();
+            console.log(transferSended)
+            // const deploy = internalWallet.deploy(bytesArray);
+            // const deploySended = await deploy.send()
+          } catch(err) {
+            console.log(err)
+          }
+        })
       }
+      const base64Key = localStorage.getItem("secretKey")
+      const bytesArray = TonWeb.utils.hexToBytes(base64Key)
+      const seqno = await internalWallet.methods.seqno().call()
+      let deploy
+      let sumOfFees = 0
+      if (!seqno) {
+        deploy = wallet.deploy(bytesArray);
+        const fee = await deploy.estimateFee()
+        sumOfFees = fee.source_fees.fwd_fee + fee.source_fees.gas_fee + fee.source_fees.in_fwd_fee
+        sumOfFees = sumOfFees / 10**9
+        console.log(sumOfFees)
+        const deployText = "Along with the deposit your in-game wallet will also be deployed. Deploy fee: " + sumOfFees.toString() + " TON (the deployment is required only 1 time)"
+        const feeInfo = document.getElementById("feeInfo")
+        feeInfo.style.display = "inherit"
+        feeInfo.innerText = deployText
+      }
+      const text = 'Your balance: ' + (balanceA / 10**9).toString() + ' TON'
+      document.getElementById("startTonBalance").style.display = 'inline-block'
+      document.getElementById("startTonBalance").innerText = text
       document.getElementById("deposit").style.display = "inline-block";
       document.getElementById("depositInput").style.display = "inline"
       document
       .getElementById("deposit")
       .addEventListener("click", async function() {
         try {
+          let internalBalanceBefore = await tonweb.getBalance(internalAddress);
+          internalBalanceBefore = internalBalanceBefore.toString()
           const wallet = localStorage.getItem('walletAddress')
           const amount = document.getElementById("depositInput").value
           const nanoAmount = toNano(amount).toString()
           const tx = await provider.send(
             'ton_sendTransaction',
             [{
-                to: wallet,
-                value: nanoAmount,
-                data: 'wallet deposit',
-                dataType: 'text'
+              to: wallet,
+              value: nanoAmount,
+              data: 'wallet deposit',
+              dataType: 'text'
             }]
           );
           document.getElementById("deposit").style.display = "none";
           document.getElementById("depositLoad").style.display = "inline-block";
           if (tx) {
-            setTimeout(() => {
-              document.getElementById("depositLoad").style.display = "none";
-              document.getElementById("deposit").style.display = "inline-block"
-              // document.getElementById("depositInput").style.display = "none"  
-              document.getElementById("room").style.display = "flex"
-              document.getElementById("name").disabled = false;
-              document.getElementById("inputName").disabled = false;
-              document.getElementById("createGame").disabled = false;
-              document.getElementById("connect").disabled = true;      
-            }, 15000)
+            let wait = async function(){
+              const internalBalanceNow = await tonweb.getBalance(internalAddress);
+              console.log(internalBalanceBefore, internalBalanceNow)
+              if(internalBalanceNow === internalBalanceBefore) {
+                ///
+              } else {
+                if (deploy) {
+                  console.log("deploy")
+                  await deploy.send()
+                }
+                document.getElementById("depositLoad").style.display = "none";
+                document.getElementById("deposit").style.display = "inline-block"
+                // document.getElementById("depositInput").style.display = "none"  
+                document.getElementById("room").style.display = "flex"
+                document.getElementById("name").disabled = false;
+                document.getElementById("inputName").disabled = false;
+                document.getElementById("createGame").disabled = false;
+                document.getElementById("connect").disabled = true;        
+                clearInterval(intervalId);
+              }
+            };
+            intervalId = setInterval(wait, 5000);
+            
           } else { 
             document.getElementById("depositLoad").style.display = "none";
             document.getElementById("deposit").style.display = "inline-block"
@@ -115,6 +175,14 @@ if (!window.location.search) {
   const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {apiKey: apiKey}));
 
   $(document).ready(async function () {
+    const tonWalletAddress = localStorage.getItem("userTonWalletAddress")
+    const tonWallet = tonweb.wallet.create({address: tonWalletAddress});
+    const tonAddress = await tonWallet.getAddress();
+    const balanceA = await tonweb.getBalance(tonAddress);
+    const text = 'Your balance: ' + (balanceA / 10**9).toString() + ' TON'
+    const tonBalance = document.getElementById("tonBalance")
+    tonBalance.style.display = "inherit"
+    tonBalance.innerText = text
     const storageWalletAddress = localStorage.getItem("walletAddress")
     const wallet = tonweb.wallet.create({address: storageWalletAddress});
     const address = await wallet.getAddress();
