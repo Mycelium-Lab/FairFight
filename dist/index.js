bdfd2dbd8752ad2babb9e7e553701bc7d9176d08
limenal <lshrims@gmail.com>
2022-07-02 16:37:05 +0500

    add in-game info

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const TonWeb = require('tonweb')
 const toNano = TonWeb.utils.toNano;
+const apiKey = 'df344c315ae71a22ae1e58ceb3158597e78435c7dac009bf30e619e3c6dca9fa'
+const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {apiKey: apiKey}));
+const provider = window.ton;
 
 if (!window.location.search) {
   $(document).ready(function () {
-    const apiKey = 'df344c315ae71a22ae1e58ceb3158597e78435c7dac009bf30e619e3c6dca9fa'
-    const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {apiKey: apiKey}));
-    const provider = window.ton;
     var walletConnect = document.getElementById("connect");
     walletConnect.addEventListener("click", async function () {
       const accounts = await provider.send('ton_requestAccounts');
@@ -28,18 +28,21 @@ if (!window.location.search) {
         const walletAddress = await wallet.getAddress();
         localStorage.setItem("walletAddress", walletAddress.toString(true, true, true));
       }
-      const text = 'Your balance: ' + (balanceA / 10**9).toString() + ' TON'
-      document.getElementById("walletHeader").innerText = text
       const internalWallet = tonweb.wallet.create({address: localStorage.getItem("walletAddress")});
       const internalAddress = await internalWallet.getAddress();
       const internalBalance = await tonweb.getBalance(internalAddress);
       const internalNumBalance = internalBalance / 10**9
       if (internalNumBalance >= 1) {
+        const text = "Your in-game balance: " + internalNumBalance.toString() + " TON"
+        document.getElementById("walletHeader").innerText = text  
         document.getElementById("room").style.display = "flex"
         document.getElementById("name").disabled = false;
         document.getElementById("inputName").disabled = false;
         document.getElementById("createGame").disabled = false;
         document.getElementById("connect").disabled = true;
+      } else {
+        const text = 'Your balance: ' + (balanceA / 10**9).toString() + ' TON'
+        document.getElementById("walletHeader").innerText = text  
       }
       document.getElementById("deposit").style.display = "inline-block";
       document.getElementById("depositInput").style.display = "inline"
@@ -106,7 +109,20 @@ if (!window.location.search) {
 
   //   window.location.search = roomName || Math.random().toString(16).slice(-10);
 } else {
-  $(document).ready(function () {
+  $(document).ready(async function () {
+    const storageWalletAddress = localStorage.getItem("walletAddress")
+    const wallet = tonweb.wallet.create({address: storageWalletAddress});
+    const address = await wallet.getAddress();
+    const balance = await tonweb.getBalance(address);
+    const numBalance = balance / 10**9
+    const textBalance = "Your in-game balance: " + numBalance.toString() + " TON"
+    const textWallet = "Your in-game wallet: " + storageWalletAddress
+    const internalBalance = document.getElementById("internalBalance")
+    internalBalance.style.display = "inherit"
+    internalBalance.innerText = textBalance
+    const internalWallet = document.getElementById("internalWallet")
+    internalWallet.style.display = "inherit"
+    internalWallet.innerText = textWallet
     document.getElementById("start").style.display = "none"; // hide
     document.getElementById("info").style.display = "inline";
     document.getElementById("canvas").style.display = "inherit"; //
