fd31db2aef5f9c2c46d7ad2db2e6f4225759d3e4
elolpuer <egorgoncharov6755@gmail.com>
2023-03-15 14:22:32 +0300

    change front index.js file on new contract

--- evm/hardhat.config.js
+++ evm/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     testnet: {
       url: "http://127.0.0.1:8545",
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "689af8efa8c651a91ad287602527f3af2fe9f6501a7ac4b061667b5a93e037fd"]
     },
     goerli: {
       url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
