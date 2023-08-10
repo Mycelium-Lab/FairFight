38c2d39f2ead34881e1ddccf204569d833ece8d9
elolpuer <egorgoncharov6755@gmail.com>
2023-08-10 13:04:00 +0300

    added new condition (wins in a row)

--- server/lootbox/service.js
+++ server/lootbox/service.js
@@ -1,25 +1,28 @@
-import { getAmountOfGames, getAmountOfKills } from "./service/conditions.js"
+import { getAmountOfGames, getAmountOfKills, getAmountOfWinsInARow } from "./service/conditions.js"
 import { CONDITION_ID, amountOfGamesForPrize, amountOfKillsForPrize } from "./constants/constants.js"
 import { createSignature, getExistedSignature } from "./service/common.js"
 
 export async function getSignature(req, res) {
-    if (req.body.condition_id == CONDITION_ID.GAMES_AMOUNT_5) {
+    if (req.body.condition_id == CONDITION_ID.GAMES_AMOUNT) {
         await getSignatureForAmountOfGames(req, res)
-    }
-    if (req.body.condition_id == CONDITION_ID.KILLS_AMOUNT_100) {
+    } else if (req.body.condition_id == CONDITION_ID.KILLS_AMOUNT) {
         await getSignatureForAmountOfKills(req, res)
+    } else if (req.body.condition_id == CONDITION_ID.GAMES_WIN_AMOUNT_IN_A_ROW) {
+        await getSignatureForWinsInARow(req, res)
+    } else {
+        res.status(400).send("This condition not exist")
     }
 }
 
 async function getSignatureForAmountOfGames(req, res) {
     try {
-        const existedSignature = await getExistedSignature(req.body.chainid, req.body.address, CONDITION_ID.GAMES_AMOUNT_5)
+        const existedSignature = await getExistedSignature(req.body.chainid, req.body.address, CONDITION_ID.GAMES_AMOUNT)
         if (Object.entries(existedSignature.signature).length) {
             res.status(200).json(existedSignature.signature)
         } else {
             const amountOfGames = await getAmountOfGames(req.body.chainid, req.body.address)
             if (amountOfGames.count >= amountOfGamesForPrize) {
-                const signature = await createSignature(req.body.chainid, req.body.address, CONDITION_ID.GAMES_AMOUNT_5)
+                const signature = await createSignature(req.body.chainid, req.body.address, CONDITION_ID.GAMES_AMOUNT)
                 if (signature.err) {
                     res.status(signature.status).send(signature.err)
                 } else {
@@ -37,13 +40,13 @@ async function getSignatureForAmountOfGames(req, res) {
 
 async function getSignatureForAmountOfKills(req, res) {
     try {
-        const existedSignature = await getExistedSignature(req.body.chainid, req.body.address, CONDITION_ID.KILLS_AMOUNT_100)
+        const existedSignature = await getExistedSignature(req.body.chainid, req.body.address, CONDITION_ID.KILLS_AMOUNT)
         if (Object.entries(existedSignature.signature).length) {
             res.status(200).json(existedSignature.signature)
         } else {
             const amountOfKills = await getAmountOfKills(req.body.chainid, req.body.address)
             if (amountOfKills.count >= amountOfKillsForPrize) {
-                const signature = await createSignature(req.body.chainid, req.body.address, CONDITION_ID.KILLS_AMOUNT_100)
+                const signature = await createSignature(req.body.chainid, req.body.address, CONDITION_ID.KILLS_AMOUNT)
                 if (signature.err) {
                     res.status(signature.status).send(signature.err)
                 } else {
@@ -51,6 +54,30 @@ async function getSignatureForAmountOfKills(req, res) {
                 }
             } else {
                 res.status(401).send("Not enough kills")
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+}
+
+async function getSignatureForWinsInARow(req, res) {
+    try {
+        const existedSignature = await getExistedSignature(req.body.chainid, req.body.address, CONDITION_ID.GAMES_WIN_AMOUNT_IN_A_ROW)
+        if (Object.entries(existedSignature.signature).length) {
+            res.status(200).json(existedSignature.signature)
+        } else {
+            const amountOfWins = await getAmountOfWinsInARow(req.body.chainid, req.body.address)
+            if (amountOfWins.allWins) {
+                const signature = await createSignature(req.body.chainid, req.body.address, CONDITION_ID.GAMES_WIN_AMOUNT_IN_A_ROW)
+                if (signature.err) {
+                    res.status(signature.status).send(signature.err)
+                } else {
+                    res.status(signature.status).json(signature.signature)
+                }
+            } else {
+                res.status(401).send("Not enough wins in a row")
             }
         }
     } catch (error) {
--- server/lootbox/service/conditions.js
+++ server/lootbox/service/conditions.js
@@ -1,4 +1,5 @@
 import db from "../../db/db.js"
+import { amountOfKillsForPrize, amountOfWinsInARowForPrize } from "../constants/constants.js"
 
 const pgClient = db()
 await pgClient.connect()
@@ -25,3 +26,39 @@ export async function getAmountOfKills(chainid, address) {
     }
 }
 
+export async function getAmountOfWinsInARow(chainid, address) {
+    try {
+        const res = await pgClient.query(`
+            SELECT s1.gameid,
+                s1.player AS player1,
+                s1.kills AS kills_player1,
+                s1.deaths AS deaths_player1,
+                s1.amount AS amount_player1,
+                s1.remainingRounds AS remainingRounds_player1,
+                s2.player AS player2,
+                s2.kills AS kills_player2,
+                s2.deaths AS deaths_player2,
+                s2.amount AS amount_player2,
+                s2.remainingRounds AS remainingRounds_player2
+            FROM statistics s1
+            JOIN statistics s2 ON s1.gameid = s2.gameid
+            WHERE s1.player = $1 AND s2.player != $1 AND s1.chainid=$2 AND s2.chainid=$2
+            ORDER BY s1.gameid DESC
+            LIMIT ${amountOfKillsForPrize};
+        `, [address, chainid])
+        let allWins = true
+        if (res.rows.length >= amountOfWinsInARowForPrize) {
+            res.rows.forEach(v => {
+                if (BigInt(v.amount_player1) <= BigInt(v.amount_player2)) {
+                    allWins = false
+                }
+            })
+        } else {
+            allWins = false
+        }
+        return { err: null, allWins }
+    } catch (error) {
+        console.log(error)
+        return { err: err.message, allWins: null }
+    }
+}
