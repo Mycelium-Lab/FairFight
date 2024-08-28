e7006db7a72f39c7a91230591793923815efb81a
elolpuer <egorgoncharov6755@gmail.com>
2024-08-28 14:34:31 +0700

    dynamiclly update open games

--- server/ton/controller.js
+++ server/ton/controller.js
@@ -4,20 +4,24 @@ import { getFights } from "./service.js";
 const tonRouter = Router()
 
 tonRouter.get('/ton/fights', async (req, res) => {
-    let fights = await getFights()
-    fights = fights.map(v => {
-        v.id = v.id.toString()
-        v.owner = v.owner.toString()
-        v.createTime = v.createTime.toString()
-        v.finishTime = v.finishTime.toString()
-        v.baseAmount = v.baseAmount.toString()
-        v.amountPerRound = v.amountPerRound.toString()
-        v.rounds = v.rounds.toString()
-        v.maxPlayersAmount = v.maxPlayersAmount.toString()
-        v.players = v.players.map(v => v.toString())
-        return v
-    })
-    res.status(200).json({fights: fights})
+    try {
+        let fights = await getFights()
+        fights = fights.map(v => {
+            v.id = v.id.toString()
+            v.owner = v.owner.toString()
+            v.createTime = v.createTime.toString()
+            v.finishTime = v.finishTime.toString()
+            v.baseAmount = v.baseAmount.toString()
+            v.amountPerRound = v.amountPerRound.toString()
+            v.rounds = v.rounds.toString()
+            v.maxPlayersAmount = v.maxPlayersAmount.toString()
+            v.players = v.players.map(v => v.toString())
+            return v
+        })
+        res.status(200).json({fights: fights})
+    } catch (error) {
+        res.status(500).json({fights: []})
+    }
 })
 
 export default tonRouter
