375f387747771b28db35a2dd5571f08e502031c6
elolpuer <egorgoncharov6755@gmail.com>
2024-11-22 21:17:09 +0700

    statistics in one call

--- server/statistics/controller.js
+++ server/statistics/controller.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCurrentInGameStatistics, getStatistics, getStatisticsByAddress } from "./service.js";
+import { getCurrentInGameStatistics, getStatistics, getStatisticsAll, getStatisticsByAddress } from "./service.js";
 
 const statisticsRouter = Router()
 
@@ -10,6 +10,11 @@ statisticsRouter.get('/statistics/all', async (req, res) => {
 
 statisticsRouter.get('/statistics', async (req, res) => {
     const data = await getStatistics(req.query.gameID, req.query.address, req.query.chainid) 
+    res.status(data.code).json(data.statistics)
+})
+
+statisticsRouter.get('/statisticsmulti', async (req, res) => {
+    const data = await getStatisticsAll(req.query.gameids.split(',').map(Number), req.query.address, req.query.chainid)
     res.status(data.code).json(data.statistics)
 })
 
